157afd19ecb59f28b199b4b05e6a191329c52558
Vũ Thiện <48660690+VuThien3010@users.noreply.github.com>
2021-01-17 05:30:12 +0700

    push

--- knnAlgorithm.py
+++ knnAlgorithm.py
@@ -31,11 +31,14 @@ def loadDataset(filename, split, trainingSet=[], testSet=[], content_header=[]):
             else:
                 testSet.append(dataset[x])
 
+
+
 def euclideanDistance(instance1, instance2, length):
     distance = 0
     for x in range(1, length):
         distance += pow((instance1[x] - instance2[x]), 2)
     return math.sqrt(distance)
+
 
 # get k nearest neighbors of the <array><num> testInstance among <array><array>
 # trainingSet
@@ -54,6 +57,7 @@ def getNeighbors(trainingSet, testInstance, k):
         neighbors.append(distance[x][0])
     return neighbors
 
+
 # make all responses vote their classification, the one with the highest vote
 # wins
 def getResponse(neighbors):
@@ -67,6 +71,7 @@ def getResponse(neighbors):
     sortedVotes = sorted(classVotes.items(), key=operator.itemgetter(1), reverse=True)
     return sortedVotes[0][0]
 
+
 def getAccuracy(testSet, predictions):
     correct = 0
     for x in range(len(testSet)):
@@ -74,20 +79,25 @@ def getAccuracy(testSet, predictions):
             correct += 1
     return (correct/float(len(testSet))) * 100.0
 
-def getAccuracy1(testSet, predictions):
-    correct = 0
-    for x in range(len(testSet)):
-        if RMSD(testSet[x][-1], predictions[x]) < 1:
-            correct += 1
-    return (correct/float(len(testSet))) * 100.0
+
+# def getAccuracy1(testSet, predictions):
+#     correct = 0
+#     for x in range(len(testSet)):
+#         if RMSD(testSet[x][-1], predictions[x]) < 1:
+#             correct += 1
+#     return (correct/float(len(testSet))) * 100.0
+
 
 def RMSD(X, Y):
     return math.sqrt(pow(Y - X, 2))
+
 
 def change(today, yest):
     if today > yest:
         return 'up'
     return 'down'
+
+
 
 def getData(filename, stockname, startdate, enddate):
     stock = web.DataReader(stockname, 'yahoo', startdate, enddate)
@@ -112,6 +122,7 @@ def getData(filename, stockname, startdate, enddate):
             stockwriter.writerow([new_format_date] + [stck_dates[i]["Open"]] +  [stck_dates[i]["High"]] + [stck_dates[i]["Low"]]  +  [stck_dates[i]["Adj Close"]] + [change(stck_dates[i]["Adj Close"], prev_closing)])
             prev_closing = stck_dates[i]["Adj Close"]
 
+
 def abc(filename, stockname, startdate, enddate):
     apple = web.DataReader(stockname, 'yahoo', startdate, enddate)
     with open(filename, 'wb') as csvfile:
@@ -119,23 +130,30 @@ def abc(filename, stockname, startdate, enddate):
         for ind in range(1, len(apple.Open)):
             stockwriter.writerow(["open: "] + [apple.Open[ind - 1]] + ["    high: "] + [apple.High[ind - 1]] + ["   low: "] + [apple.Low[ind - 1]] + ["  yester close: "] + [apple['Adj Close'][ind - 1]] + [" volume: "] + [apple.Volume[ind - 1]] + [change(apple['Adj Close'][ind], apple['Adj Close'][ind - 1])])
 
+
+
 def predictFor(k, filename, stockname, startdate, enddate, writeAgain, split):
     iv = ["date", "open", "high", "low", "yesterday closing adj", "state change"]
     trainingSet = []
     testSet = []
     totalCount = 0
+
     if writeAgain:
         print("making a network request")
         getData(filename, stockname, startdate, enddate)
+
     # open the file
     loadDataset(filename, split, trainingSet, testSet, iv)
+
     print("Predicting for ", stockname)
     print("Train: " + repr(len(trainingSet)))
     print("Test: " + repr(len(testSet)))
     totalCount += len(trainingSet) + len(testSet)
     print("Total: " + repr(totalCount))
+
     # generate predictions
     predict_and_get_accuracy(testSet, trainingSet, k, stockname)
+
 
 def predict_and_get_accuracy(testSet, trainingSet, k, stockname):
     predictions = []
@@ -143,8 +161,10 @@ def predict_and_get_accuracy(testSet, trainingSet, k, stockname):
         neighbors = getNeighbors(trainingSet, testSet[x], k)
         result = getResponse(neighbors)
         predictions.append(result)
+
     accuracy = getAccuracy(testSet, predictions)
     print('Accuracy: ' + repr(accuracy) + '%')
+
     # drawing another
     plt.figure(2)
     plt.title("Prediction vs Actual Trend of " + stockname)
@@ -159,6 +179,7 @@ def predict_and_get_accuracy(testSet, trainingSet, k, stockname):
         else:
             col.append(1)
     predicted_plt, = plt.plot(row, col, 'r', label="Predicted Trend")
+
     row = []
     col = []
     for dates in range(len(testSet)):
@@ -169,8 +190,12 @@ def predict_and_get_accuracy(testSet, trainingSet, k, stockname):
         else:
             col.append(1)
     actual_plt, = plt.plot(row, col, 'b', label="Actual Trend")
+
     plt.legend(handles=[predicted_plt, actual_plt])
+
+
     plt.show()
+
 
 def main():
     split = 0.67
@@ -178,11 +203,12 @@ def main():
     startdate = datetime.datetime(2002,1,1)
     enddate = datetime.date.today()
 
-    predictFor(5, 'amtd.csv', 'AMTD', startdate, enddate, 1, split)
+    #predictFor(5, 'amtd.csv', 'AMTD', startdate, enddate, 1, split)
     predictFor(5, 'amazon.csv', 'AMZN', startdate, enddate, 1, split)
     predictFor(5, 'disney.csv', 'DIS', startdate, enddate, 1, split)
     predictFor(5, 'sbux.csv', 'SBUX', startdate, enddate, 1, split)
     predictFor(5, 'twlo.csv', 'TWLO', startdate, enddate, 1, split)
     predictFor(5, 'twtr.csv', 'TWTR', startdate, enddate, 1, split)
+    predictFor(5, 'yahoo.csv', 'YHOO', startdate, enddate, 1, split)
 
 main()
